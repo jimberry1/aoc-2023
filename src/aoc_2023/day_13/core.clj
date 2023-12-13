75ed0f50df403a40846209c28563793d095bbcd8
jim.berry <jim.berry@signal-ai.com>
2023-12-13 19:53:25 +0000

    abstracting differences to difference count

--- src/aoc_2023/day_13/core.clj
+++ src/aoc_2023/day_13/core.clj
@@ -10,12 +10,7 @@
       (recur (map rest all-rows) (conj columns (apply str (map first all-rows)))))))
 
 (defn compare-with-smudge [str-1 str-2]
-  (let [num-of-entries (count str-1)
-        equal-count (->> (map #(= %1 %2) str-1 str-2)
-                         (filter true?)
-                         count)]
-    {:equal? (= equal-count num-of-entries)
-     :equal-with-smudge? (>= (inc equal-count) num-of-entries)}))
+  (->> (map #(= %1 %2) str-1 str-2) (remove identity) count))
 
 (defn is-smudged-mirror-img?
   "Checks to see whether two groups are exactly 1 smudge away from being enantiomers"
@@ -23,13 +18,12 @@
   (let [shortest-coll-length (min (count group-1) (count group-2))
         normalised-group-1 (->> group-1 reverse (take shortest-coll-length)) ;; reversed for looping simplicity
         normalised-group-2 (take shortest-coll-length group-2)]
-    (loop [[first-1 & rest-1] normalised-group-1 [first-2 & rest-2] normalised-group-2 already-used-smudge? false]
-      (let [{:keys [equal? equal-with-smudge?]} (compare-with-smudge first-1 first-2)]
+    (loop [[first-1 & rest-1] normalised-group-1 [first-2 & rest-2] normalised-group-2 total-differences 0]
+      (let [differences-count (compare-with-smudge first-1 first-2)]
         (cond
-          (nil? first-1) already-used-smudge? ;; when seq exhausted return whether you used the smudge
-          (not equal-with-smudge?) false
-          equal? (recur rest-1 rest-2 already-used-smudge?)
-          (and equal-with-smudge? (not already-used-smudge?)) (recur rest-1 rest-2 true))))))
+          (nil? first-1) (= 1 total-differences) ;; when seq exhausted return whether you used the smudge
+          (> differences-count 1) false
+          :else (recur rest-1 rest-2 (+ total-differences differences-count)))))))
 
 (defn is-mirror-img?
   "Checks whether two groups are mirror images of each other.
