ef1e52a9dd3a57c9d0540670d61237cc49aed519
jim.berry <jim.berry@signal-ai.com>
2023-12-15 09:24:01 +0000

    day 14 / 15 template

---
+++ src/aoc_2023/day_14/core.clj
@@ -0,0 +1,6 @@
+(ns aoc-2023.day-14.core
+  (:require [clojure.string :as str]
+            [aoc-2023.utils :as utils]))
+
+(def test-input (utils/input-file->rows "src/aoc_2023/day_14/example.txt"))
+(def real-input (utils/input-file->rows "resources/input_14.txt"))
---
+++ src/aoc_2023/day_15/core.clj
@@ -0,0 +1,6 @@
+(ns aoc-2023.day-15.core
+  (:require [clojure.string :as str]
+            [aoc-2023.utils :as utils]))
+
+(def test-input (utils/input-file->rows "src/aoc_2023/day_15/example.txt"))
+(def real-input (utils/input-file->rows "resources/input_15.txt"))
